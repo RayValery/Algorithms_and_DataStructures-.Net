8ffe10cf992d29bf7f848e459bbe3775d7a85368
Valerie Salivon <ValerieSalivon@gmail.com>
2019-06-28 12:44:00 +0300

    Add files via upload
    Description of the type of nodes in the list

---
+++ Node.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica
+{
+    public class Node : Data
+    {
+
+    private Node next;
+    private Node previous;
+
+    private Data myStudent = new Data();
+
+    public Node(Data Student)
+    {
+        this.myStudent.setArmy(Student.getArmy());
+        this.myStudent.setCourse(Student.getCourse());
+        this.myStudent.setName(Student.getName());
+        this.next = null;
+        this.previous = null;
+    }
+
+    public String valueName()
+    {
+        return myStudent.getName();
+    }
+
+    public int valueCourse()
+    {
+        return myStudent.getCourse();
+    }
+
+    public Boolean valueArmy()
+    {
+        return myStudent.getArmy();
+    }
+
+    public Node getNext()
+    {
+        return next;
+    }
+
+    public void setNext(Node next)
+    {
+        this.next = next;
+    }
+
+    public Node getPrevious()
+    {
+        return previous;
+    }
+
+    public void setPrevious(Node previous)
+    {
+        this.previous = previous;
+    }
+
+    public Data getMyStudent()
+    {
+        return myStudent;
+    }
+
+    public void setMyStudent(Data myStudent)
+    {
+        this.myStudent = myStudent;
+    }
+
+    public String toString()
+    {
+        return "Name: " + myStudent.getName() +
+                "\nCourse: " + myStudent.getCourse() +
+                "\nArmy: " + myStudent.getArmy() + "\n";
+    }
+}
+}
