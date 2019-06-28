e45213d26f630ca8df7e8022573b4f4937af22be
Valerie Salivon <ValerieSalivon@gmail.com>
2019-06-28 12:42:55 +0300

    Add files via upload
    Description of the type of nodes in the list

---
+++ Data.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica
+{
+    public class Data
+    {
+
+        private String name;
+        private int course;
+        private Boolean army;
+
+        public Data()
+        {
+            name = "";
+            course = 0;
+            army = false;
+        }
+
+        public Data(String name, int course, Boolean army)
+        {
+            this.name = name;
+            this.course = course;
+            this.army = army;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public int getCourse()
+        {
+            return course;
+        }
+
+        public void setCourse(int course)
+        {
+            this.course = course;
+        }
+
+        public Boolean getArmy()
+        {
+            return army;
+        }
+
+        public void setArmy(Boolean army)
+        {
+            this.army = army;
+        }
+    }
+}
